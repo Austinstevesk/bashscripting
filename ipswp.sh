69d2a6c8d93c985cd4e06d5c788831ba14717ed1
Austinstevesk <austinstevesk@gmail.com>
2021-05-08 00:29:56 +0300

    Initial commit

--- ipswp.sh
+++ ipswp.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
 
+#!/bin/bash
+if ["$1" == ""]
+then
+echo "You forgot an IP address"
+echo "Syntax -> ./ipswp.sh 172.16.40"
+
+else
 for ip in `seq 1 254`; do
 ping -c 3 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
+fi
 
-./ipsweep.sh 172.16.40
